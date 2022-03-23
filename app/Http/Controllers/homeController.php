7d2dda8315edbe19e8428496fe5962524f152215
Wahyu-RizkyA <2041720191@student.polinema.ac.id>
2022-03-23 15:45:18 +0700

    js6 done

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        return view('home',['user' => $user]);
     }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 <div class="container"><br><br><br><br><br><br><br><br><br>
     <div class="row justify-content-center">
         <div class="col-md-8">
+
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -15,6 +16,15 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <table class="table table-responsive">
+                    <table class="table table-responsive"> 
+                        <tr><th>Username</th><th>:</th><td>{{ $user->username }}</td></tr>
+                        <tr><th>Name</th><th>:</th><td>{{ $user->name }}</td></tr>
+                        <tr><th>Email</th><th>:</th><td>{{ $user->email }}</td></tr>
+                        <tr><th>Created At</th><th>:</th><td>{{ $user->created_at }}</td></tr>
+                    </table>
+
                 </div>
             </div>
         </div>
